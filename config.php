f5d30fddfea2b63f9a561874b3392b149e304de1
Cristiano Trevizam <cristrevizam@gmail.com>
2026-06-29 17:09:10 -0300

    refactor: move configuracao legada para config

--- config.php
+++ config.php
@@ -1,20 +1,2 @@
 <?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-$session_timeout = 900; // 900 segundos = 15 minutos
-$domains = [];
-
-foreach (glob("/var/cache/bind/master-aut/*.hosts") as $f) {
-
-    if (basename($f) == ".hosts") {
-        continue;
-    }
-
-    $domain = basename($f, ".hosts");
-    $domains[$domain] = $f;
-}
-
-
-ksort($domains);
-?>
+require_once __DIR__ . '/config/app.php';
